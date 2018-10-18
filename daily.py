a8f4815d02028c1047141a7869fe424a04f49d26
Qingslev <43660763+Qing1998@users.noreply.github.com>
2018-10-18 09:17:34 +0800

    Update daily.py
    - 现在新成员不需要手动添加 只需要遍历
    - 优化了有关member_list的代码

--- daily.py
+++ daily.py
@@ -18,7 +18,7 @@ id date potato presence
 一些常见变量名的含义
 member_list　正在组里的成员组成的表
 date 前一天，即要处理的数据对应的日期
-daily_list 当天的报表
+daily_tuples 当天的报表
 """
 
 def getDate():
@@ -26,12 +26,29 @@ def getDate():
     date=(now-timedelta(days=1)).strftime('%y%m%d')
     return date
 
-def initNameTable():
-    name=input('Name?')
-    name=re.sub('[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”:：！，。？、~@#￥%……&*（）]+', '',name)
-    logging.info('name=%s',name)
-    cursor.execute('CREATE TABLE T_'+name+' (id INTEGER PRIMARY KEY AUTOINCREMENT,date CHAR(6),potato CHAR(2),presence CHAR(1))')
-    cursor.execute('INSERT INTO T_Member VALUES(NULL,"'+name+'")')
+def keepOn(s):
+    print(s)
+    answer=input('Go on?')
+
+def getMemberList():
+    cursor.execute('SELECT name FROM T_Member')
+    member_tuples = cursor.fetchall()  # 这是个包含tuple的list
+    member_list=[]
+    for x in member_tuples:
+        member_list.append(x[0])
+    return member_list
+
+def initNameTable(daily_tuples,member_list):
+    member_list_today = []
+    for x in daily_tuples:
+        member_list_today.append(x[0])
+    for x in member_list_today:
+        if x in member_list:
+            pass
+        else:
+            keepOn('New member:'+x)
+            cursor.execute('CREATE TABLE T_'+x+' (id INTEGER PRIMARY KEY AUTOINCREMENT,date CHAR(6),potato CHAR(2),presence CHAR(1))')
+            cursor.execute('INSERT INTO T_Member VALUES(NULL,"'+x+'")')
 
 def readDailyInput(date):
     pathi='/projects/SG/data/'+date+'.md'
@@ -53,7 +70,7 @@ def readDailyInput(date):
         count=count+1
     return sorted(daily_tuples, key=lambda member: member[1],reverse=True)
 
-def writeDailyTable(daily_tuples,date):
+def writeDailyTable(daily_tuples,date,member_list):
     # cursor.execute('DROP TABLE T_'+date)
     cursor.execute('CREATE TABLE T_'+date+' (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,potato CHAR(2),presence CHAR(1))')#0 False 1 True
     for x in daily_tuples:
@@ -65,43 +82,38 @@ def writeDailyTable(daily_tuples,date):
     member_list_today=[]
     for x in daily_tuples:
         member_list_today.append(x[0])
-    cursor.execute('SELECT name FROM T_Member')
-    member_list=cursor.fetchall() #这是个包含tuple的list
+
     for x in member_list:
-        if x[0] in member_list_today:
+        if x in member_list_today:
             pass
         else:
-            reason=input('Does '+x[0]+' has asked for leave or is absent?(L/A)')
+            reason=input('Does '+x+' has asked for leave or is absent?(L/A)')
             assert reason=='L'or reason=='A','wrong input!'
             if reason=='L':
                 cursor.execute("INSERT INTO T_"+date+" VALUES(NULL,'"+x[0]+"','0','1')")
             elif reason=='A':
                 cursor.execute("INSERT INTO T_"+date+" VALUES(NULL,'"+x[0]+"','0','0')")
 
-def writeNameTable(date):
-    cursor.execute('SELECT name FROM T_Member')
-    member_list=cursor.fetchall()
+def writeNameTable(date,member_list):
     cursor.execute('SELECT name,potato,presence FROM T_'+date)
     daily_list=cursor.fetchall()
     for x in member_list:
         for xn in daily_list:#([name potato presence],[]...)
-            if x[0]==xn[0]:
+            if x==xn[0]:
                 cursor.execute('INSERT INTO T_'+xn[0]+' VALUES (NULL,"'+date+'","'+xn[1]+'","'+xn[2]+'")')#date potato presence
 
-def getQuitMember():
-    cursor.execute('SELECT name FROM T_Member')
-    member_list=cursor.fetchall()
+def getQuitMember(member_list):
     quit_member=[]
     for x in member_list:
-        cursor.execute('SELECT presence FROM T_'+x[0])
-        presence_list=cursor.fetchall()
+        cursor.execute('SELECT presence FROM T_'+x)
+        presence_tuples=cursor.fetchall()
         absence=0
-        for i in presence_list:
+        for i in presence_tuples:
             if i[0]=='0':
                 absence+=1
         if absence>=2:
-            print('%s should quit.',x[0])
-            quit_member.append(x[0])
+            keepOn('Quit Member:'+x)
+            quit_member.append(x)
     return quit_member
 
 def writeDailyOutput(date,quit_member):
@@ -110,7 +122,7 @@ def writeDailyOutput(date,quit_member):
     cursor.execute('SELECT name,potato FROM T_' + date)
     daily_list = cursor.fetchall()
     fo.write(date+'\n\n')
-    fo.write('Rank | Name | Potato\n')
+    fo.write('Rank | Name | Todo\n')
     fo.write('-----|------|------\n')
     for i,x in enumerate(daily_list):
         fo.write(str(i+1)+' | '+x[0]+' | '+x[1]+'\n')
@@ -126,34 +138,31 @@ if __name__ =='__main__':
     conn = sqlite3.connect('sg.db')
     cursor = conn.cursor()
     date=getDate()
+    keepOn('Good Morning!')
 
-    print('Good Morning!')
-
-    answer='Y'
-    while answer=='Y':
-        answer=input('Is there any new member?(Y/N)')
-        assert answer=='Y' or answer=='N','wrong input!'
-        if answer=='Y':
-            initNameTable()
-
-
-
-    s=readDailyInput(date)
+    l=readDailyInput(date)
     print('Successfully Input the Data!')
-    print(s)
 
-    writeDailyTable(s,date)
+    m=getMemberList()
+    initNameTable(l,m)
+    m=getMemberList()
+    print('Successfully Init Name Table!')
 
-    writeNameTable(date)
+    writeDailyTable(s,date,m)
+    print('Successfully Write Daily Table!')
 
-    quit_member=getQuitMember()
+    writeNameTable(date,m)
+    print('Successfully Write Name Table!')
+
+    quit_member=getQuitMember(m)
+    print('Successfully Get Quit Member!')
 
     writeDailyOutput(date,quit_member)
     print('Successfully Output the Daily Table!')
 
     for x in quit_member:
         dropNameTable(x)
-    print('Successfully Drop the Table of the quited members')
+    print('Successfully Drop the Table of the quited members!')
 
     cursor.close()
     conn.commit()
